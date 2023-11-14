b780d0cc4c4cd441eebac79eed1b869753d1ac09
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2023-11-14 12:07:56 -0300

    Correção concluido e melhorado o EX.24

--- 02 - Estrutura_de_Decisão/24.py
+++ 02 - Estrutura_de_Decisão/24.py
@@ -1,5 +1,5 @@
 """
-25 - Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
+24 - Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
 par ou ímpar;
 positivo ou negativo;
 inteiro ou decimal.
