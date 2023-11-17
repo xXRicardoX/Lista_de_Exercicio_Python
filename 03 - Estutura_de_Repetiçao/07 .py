69ff92369ae9d4776ab64da34bfd8c9108f28c56
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2023-11-17 15:06:10 -0300

    Ex.3 concluido

--- 03 - Estutura_de_Repetiçao/07 .py
+++ 03 - Estutura_de_Repetiçao/07 .py
@@ -1,2 +1,10 @@
 # 07 - Faça um programa que leia 5 números e informe o maior número.
 
+for item in range(5):
+    numero = int(input("Digite um numero:"))
+    if item == 0:
+        maior = numero
+    else:
+        if numero > maior:
+            maior = numero
+print("O maior numero é:", maior)
