f95dbc56596e0d6503415acf82ec29d3bb981b3e
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2023-11-18 23:38:53 -0300

    Exer.18 Exercicio resolvido

---
+++ 03 - Estutura_de_Repetiçao/18.py
@@ -0,0 +1,28 @@
+conjunto_numerico = int(input("Digite a quantidade de valores para somar: "))
+soma = 0
+
+for item in range(conjunto_numerico):
+    # Inserindo os valores
+    numero = int(input("Digite um numero:"))
+    
+    
+    # verificando o maior numero
+    if item == 0:
+        maior = numero
+    else:
+        if numero > maior:
+            maior = numero
+    # Verificando os menores numeros
+    if item == 0:
+        menor = numero
+    else:
+        if numero < menor:
+            menor = numero
+    # Somando os numero digitados
+    soma += numero
+    
+    
+    
+print(f"O maior numero é: {maior}")
+print(f"O menor numero é: {menor}")
+print(f"A soma dos numeros é: {soma}")
