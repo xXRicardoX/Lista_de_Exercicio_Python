be63e84cad69440bc51c0b0d79ee66690cab3a32
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2023-11-18 23:41:15 -0300

    Exer.18 concluido

--- 03 - Estutura_de_Repetiçao/18.py
+++ 03 - Estutura_de_Repetiçao/18.py
@@ -1,3 +1,5 @@
+"""18 - Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores."""
+
 conjunto_numerico = int(input("Digite a quantidade de valores para somar: "))
 soma = 0
 
