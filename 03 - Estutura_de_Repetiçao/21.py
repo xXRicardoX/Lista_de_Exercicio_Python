c8880bd21fc8ba05acedb3f664b479eff8963b14
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2023-11-23 00:10:32 -0300

    Adicionado o enunciado

--- 03 - Estutura_de_Repetiçao/21.py
+++ 03 - Estutura_de_Repetiçao/21.py
@@ -1,4 +1,6 @@
-
+"""
+21 - Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.
+"""
 
 numero = int(input("Digite um numero para verificar se são numeros primos ou não: "))
 
