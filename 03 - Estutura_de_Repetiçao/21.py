eb0a78c6420c5d6063b8f5c96f16b1fe83a6c0d6
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2023-11-23 01:04:06 -0300

    Exer.21 consertado

--- 03 - Estutura_de_Repetiçao/21.py
+++ 03 - Estutura_de_Repetiçao/21.py
@@ -1,15 +1,13 @@
 """
 21 - Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.
 """
-
 numero = int(input("digite um valor: "))
-
 
 if numero < 2:
     print("Não é primo")
 else:
     for i in range(2, int(numero**0.5) + 1):
-        if numero % i == 0:           
+        if numero % i == 0:
             print("Não é primo")
             break
     else:
